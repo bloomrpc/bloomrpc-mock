43ffdc237f6283ece441bc0b840925d572a23fee
Ivan Sabelnikov <ivan@mapped.com>
2021-05-02 11:29:09 +0300

    Fix infinite loop another ways

--- src/protobuf.ts
+++ src/protobuf.ts
@@ -131,7 +131,7 @@ export function matchingAncestorNamespaceLookup(typeName: string, parentNamespac
 }
 
 export function walkNamespace(root: Root, onNamespace: (namespace: Namespace) => void, parentNamespace?: Namespace) {
-  const nestedType = (parentNamespace && parentNamespace.nested) || root.nested;
+  const nestedType = parentNamespace ? parentNamespace.nested : root.nested;
 
   if (nestedType) {
     Object.keys(nestedType).forEach((typeName: string) => {
@@ -141,11 +141,7 @@ export function walkNamespace(root: Root, onNamespace: (namespace: Namespace) =>
       const nestedNamespace = root.lookup(typeName);
       if (nestedNamespace && isNamespace(nestedNamespace)) {
         onNamespace(nestedNamespace as Namespace);
-
-        // Do not search inside of empty namespaces
-        if ((nestedNamespace as Namespace).nested) {
-          walkNamespace(root, onNamespace, nestedNamespace as Namespace);
-        }
+        walkNamespace(root, onNamespace, nestedNamespace as Namespace);
       }
     });
   }
